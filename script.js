e7a97751f855a6ea395e421c7c00b0cc49468073
Brent Biglin <biglin@gmail.com>
2017-11-26 13:55:26 -0600

    make it grow

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-  var width = 90;
+  var width = 4;
   // bottom border width for h1
   var hero = document.getElementById("hero");
   var t = setInterval(breathe, 10);
 
 function breathe() {
-  if(width = 4) {
+  if(width >= 48) {
     clearInterval(t);
     }
     else {
-      width -= .5;
+      width += .5;
       hero.style.borderBottomWidth = width+"px";
     }
   };
