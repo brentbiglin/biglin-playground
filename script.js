a0c2c553e3f339ea97e0726c8342fbf394150f96
Brent Biglin <biglin@gmail.com>
2017-11-26 15:05:13 -0600

    make it loop

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ document.getElementById("hero").onclick = function grow() {
   if(width >= 28) {
       hero.style.borderBottomWidth = 4+"px";
       hero.style.borderTopWidth = 4+"px";
+      continue;
     }
     else {
       width += 1;
