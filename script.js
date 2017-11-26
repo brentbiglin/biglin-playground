321b2ba44285dd1bd858bda7cbce87d9f1251dbe
Brent Biglin <biglin@gmail.com>
2017-11-26 14:39:14 -0600

    go slower

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var width = 4;
 // bottom border width for h1
 var hero = document.getElementById("hero");
-var t = setInterval(grow, 10);
+var t = setInterval(grow, 1000);
 
 function grow() {
   if(width >= 8) {
     clearInterval(t);
-    var t = setInterval(shrink, 10);
+    var t = setInterval(shrink, 1000);
     }
     else {
       width += .5;
@@ -18,7 +18,7 @@ function grow() {
 function shrink() {
   if(width <= 1) {
     clearInterval(t);
-    var t = setInterval(grow, 10);
+    var t = setInterval(grow, 1000);
     }
     else {
       width -= .5;
