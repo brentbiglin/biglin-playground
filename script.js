07256696a4ade777579b417b2f2e91edab7689d1
Brent Biglin <biglin@gmail.com>
2017-11-26 11:34:44 -0600

    adding equal to sign

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var border = document.getElementsByTagName("h1");
 var t = setInterval(breathe, 10);
 
 function breathe() {
-  if (width > 4) {
+  if (width >= 4) {
     width -= .5;
     h1.borderBottomWidth = width+"px";
     }
