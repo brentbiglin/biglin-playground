62263139ffb63b32a7389b57e2ca33c8fcce8215
Brent Biglin <biglin@gmail.com>
2017-11-26 14:05:47 -0600

    interval around function

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
   // bottom border width for h1
   var hero = document.getElementById("hero");
 
+setInterval(
 function breathe() {
   if(width >= 48) {
       width -= .5;
@@ -11,4 +12,4 @@ function breathe() {
       width += .5;
       hero.style.borderBottomWidth = width+"px";
     }
-  };
+  };, 3000)
