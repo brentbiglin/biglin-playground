420ef19d77853362ba5c5945a47e5c9b07cf4725
Brent Biglin <biglin@gmail.com>
2017-11-26 14:29:19 -0600

    refer to intervals by variables

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
-  var width = 4;
-  // bottom border width for h1
-  var hero = document.getElementById("hero");
-  var t1 = setInterval(grow, 10);
-  var t2 = setInterval(shrink, 10);
+var width = 4;
+// bottom border width for h1
+var hero = document.getElementById("hero");
+var t1 = setInterval(grow, 10);
+var t2 = setInterval(shrink, 10);
 
 function grow() {
   if(width >= 36) {
     clearInterval(t1);
-    setInterval(shrink, 10);
+    setInterval(t2);
     }
     else {
       width += .5;
@@ -15,10 +15,11 @@ function grow() {
     }
   };
 
+
 function shrink() {
   if(width <= 1) {
     clearInterval(t2);
-    setIntervial(grow, 10);
+    setIntervial(t1);
     }
     else {
       width -= .5;
