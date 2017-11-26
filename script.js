69d38383acf1773df53f6014644396c972c36a38
Brent Biglin <biglin@gmail.com>
2017-11-26 14:16:18 -0600

    grow and shrink

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
   var width = 4;
   // bottom border width for h1
   var hero = document.getElementById("hero");
-  var t = setInterval(breathe, 10);
+  var t1 = setInterval(grow, 10);
+  var t2 = setInterval(shrink, 10);
 
-function breathe() {
+function grow() {
   if(width >= 36) {
-    clearInterval(t);
+    clearInterval(t1);
     }
     else {
       width += .5;
+      hero.style.borderBottomWidth = width+"px";
+    }
+  };
+
+function shrink() {
+  if(width <= 1) {
+    clearInterval(t2);
+    }
+    else {
+      width -= .5;
       hero.style.borderBottomWidth = width+"px";
     }
   };
