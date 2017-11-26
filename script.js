ca06b3213954dccbd0bf66335f60a300474f1767
Brent Biglin <biglin@gmail.com>
2017-11-26 11:24:17 -0600

    first try h1 width

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-alert("Test test!");
-document.querySelector('html').onclick = function() {
-    alert('Ouch! Stop poking me!');
-}
+var width = 4;
+// bottom border width for h1
+var border = document.getElementByTagName("h1");
+var t = setInterval(grow, 10);
+
+function grow() {
+  if (width > 4) {
+    width -= .5;
+    h1.borderBottom = width+"px solid #715ACE;";
+    }
+    else if (width <= 0) {
+      width += .5;
+      h1.borderBottom = width+"px solid #715ACE;";
+    }
+  }
